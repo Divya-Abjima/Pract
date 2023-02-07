95b4cb4fff91e7cb1a47e783eb48768a4c7e2998
Divya-Abjima <121873267+Divya-Abjima@users.noreply.github.com>
2023-02-07 18:55:28 +0530

    Add files via upload
    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http'
-import { AppRoutingModule } from './app-routing.module';
+
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { InteractComponent } from './interact/interact.component';
-import { PipesComponent } from './pipes/pipes.component';
-import { EmplistComponent } from './emplist/emplist.component';
-import { EmpdetailComponent } from './empdetail/empdetail.component';
-import { EmployeeService } from './employee.service';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    InteractComponent,
-    PipesComponent,
-    EmplistComponent,
-    EmpdetailComponent
+    routingComponents
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
+    AppRoutingModule
   ],
-  providers: [EmployeeService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
