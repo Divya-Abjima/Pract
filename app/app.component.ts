d0944357075031cab7eb661f6e66e4ed65501a9e
Divya-Abjima <121873267+Divya-Abjima@users.noreply.github.com>
2023-02-11 18:46:43 +0530

    Add files via upload
    response driven form validation

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, Validators, FormArray } from '@angular/forms';
 import { AbstractControlOptions } from '@angular/forms';
 import { forbiddenNameValidation } from './shared/namevalidator';
 import { passwordValidation } from './shared/passwordValidation';
@@ -12,6 +12,19 @@ import { passwordValidation } from './shared/passwordValidation';
 })
 export class AppComponent {
   title = 'form';
+  // registerForm!: FormGroup;
+
+  get email() {
+    return this.registerForm.get('email');
+  }
+
+  get alternateEmails() {
+    debugger
+    return this.registerForm.get('alternateEmails') as unknown as FormArray;
+  }
+   addAlternateEmail() {
+    this.alternateEmails.push(this.fb.control(''));
+  }
 
   constructor(private fb: FormBuilder) { }
 
@@ -29,6 +42,8 @@ export class AppComponent {
 
   registerForm = this.fb.group({
     username: ['', [Validators.required, Validators.minLength(3), forbiddenNameValidation(/admin/)]], //required field validation
+    email: [''],
+    subscribe: [false],
     password: [''],
     confirmPwd: [''],
     address: this.fb.group({
@@ -36,19 +51,33 @@ export class AppComponent {
       street: [''],
       city: [''],
       state: ['']
-    })
-  }, 
-  {validators: passwordValidation } as AbstractControlOptions );
+    }),
+    alternativeEmails: this.fb.array([])
+  },
+    { validators: passwordValidation } as AbstractControlOptions);
 
+  // this.registerForm.get('subscribe')?.valueChanges
+  //   .subscribe(checkedValue => {
+  //     const email = this.registerForm.get('email');
+  //     if (checkedValue) {
+  //       email?.setValidators(Validators.required);
+  //     }
+  //     else {
+  //       email?.clearValidators();
+  //     }
+  //     email?.updateValueAndValidity();
+  //   })
+
+  // }
   demoData() {
-    // this.registerForm.patchValue({
-    //   username : 'test',
-    //   password : '12345',
-    //   confirmPwd : '12345',
-    //   address: {
-    //     plotNo : 'test',
-    //     state : 'test'
-    //   }
-    // });
+    //   // this.registerForm.patchValue({
+    //   //   username : 'test',
+    //   //   password : '12345',
+    //   //   confirmPwd : '12345',
+    //   //   address: {
+    //   //     plotNo : 'test',
+    //   //     state : 'test'
+    //   //   }
+    //   // });
   }
 }
